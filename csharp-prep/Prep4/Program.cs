3cbd023901fcf2b97e0ab9a88896a86d07acdefb
camellecastro <camelleccastro@gmail.com>
2023-09-16 11:33:08 -0600

    Changed program to a number list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -38,18 +38,46 @@ class Program
         float average = ((float)sum) / numbers.Count;
         Console.WriteLine($"The average is: {average}");
 
-        // Part 3 - Max
+        // Part 3 - Max or Largest Number and stretch challenge minimum
         int max = numbers[0];
-
+        int minPositive = 0;
         foreach (int number in numbers)
         {
             if (number > max)
             {
                 max = number;
             }
+            else if (number > 0 && minPositive == 0)
+            {
+                minPositive = number;
+            }
+            else if (number > 0 && number < minPositive)
+            {
+                minPositive = number;
+            }
         }
 
-        Console.WriteLine($"The max is: {max}");
+        Console.WriteLine($"The largest number is: {max}");
+
+        if (minPositive == 0)
+        {
+            Console.WriteLine("There's no positive numbers entered.");
+        }
+        else
+        {
+            Console.WriteLine ($"The smallest positive number is: {minPositive}");
+        }
+
+        //stretch challenge
+
+        //Sort
+        Console.WriteLine ("The sorted list is: ");
+        numbers.Sort();
+
+        foreach (int number in numbers)
+        {
+            Console.WriteLine (number);
+        }
     }
     
 }
