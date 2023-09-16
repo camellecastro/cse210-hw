77c4b7e2bac4c21bc80a33f5830210319ce47af6
camellecastro <camelleccastro@gmail.com>
2023-09-15 23:34:00 -0600

    Updated program to introduce oneself like the iconic line of James Bond

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write ("What is your first name? ");
+        string firstname = Console.ReadLine () ;
+
+        Console.Write ("What is your last name? ");
+        string lastname = Console.ReadLine () ;
+
+        Console.WriteLine ("");
+
+        Console.WriteLine ($"Your name is {lastname}, {firstname} {lastname}.") ;
+
     }
 }
