3386473899d53d4dc9181e2329115651eef4e040
camellecastro <camelleccastro@gmail.com>
2023-09-20 13:40:46 -0600

    abstraction principle practice - Dylan job history

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Sales Development";
+        job1._company = "Workstream";
+        job1._startYear = 2023;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Co-Founder";
+        job2._company = "HireWay";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Job job3 = new Job();
+        job3._jobTitle = "Investor";
+        job3._company = "Campus Founders Fund";
+        job3._startYear = 2022;
+        job3._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Dylan Kirkland";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume._jobs.Add(job3);
+
+        myResume.Display();
     }
 }
